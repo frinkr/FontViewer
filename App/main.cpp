d74ec32f6ff4330f8a681c55eba66b738c0ca2eb
Yuqing Jiang <frinkr@outlook.com>
2018-04-19 17:13:39 +0800

    mac bundle

--- App/main.cpp
+++ App/main.cpp
@@ -1,13 +1,9 @@
-#include <iostream>
-#include <zlib.h>
-#include "UI-Qt/QUMain.h"
 #include "FontX/FXLib.h"
+#include "UI-Qt/QUMain.h"
 
 int main(int argc, char *argv[])
 {
     FXLib::init(FX_RESOURCES_DIR "/UCD");
-    
     quMain(argc, argv);
-    
     FXLib::finish();
 }
