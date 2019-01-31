95637a27a494a42ed09ff26347813896e94e1d3c
frinkr <frinkr@outlook.com>
2019-01-31 17:14:07 +0800

    move window by toolbar for windows

--- UI-Qt/QUToolBar.cpp
+++ UI-Qt/QUToolBar.cpp
@@ -1,9 +1,12 @@
 #include <QMouseEvent>
 #include "QUToolBar.h"
 
+#if defined(Q_OS_MAC) || defined(Q_OS_WIN)
+#  define QX_MOVE_WINDOW_BY_TOOLBAR
+#endif
 void
 QUToolBar::mousePressEvent(QMouseEvent * event) {
-#ifdef Q_OS_MAC
+#ifdef QX_MOVE_WINDOW_BY_TOOLBAR
     if ((event->buttons() & Qt::LeftButton)) {
         mousePressPos_ = event->pos();
         movingWindow_ = true;
@@ -14,7 +17,7 @@ QUToolBar::mousePressEvent(QMouseEvent * event) {
 
 void
 QUToolBar::mouseMoveEvent(QMouseEvent * event) {
-#ifdef Q_OS_MAC
+#ifdef QX_MOVE_WINDOW_BY_TOOLBAR
     if (movingWindow_ && (event->buttons() & Qt::LeftButton)) {
         QPoint diff = event->pos() - mousePressPos_;
         QPoint newpos = this->window()->pos() + diff;
@@ -26,7 +29,7 @@ QUToolBar::mouseMoveEvent(QMouseEvent * event) {
 
 void
 QUToolBar::mouseReleaseEvent(QMouseEvent * event) {
-#ifdef Q_OS_MAC
+#ifdef QX_MOVE_WINDOW_BY_TOOLBAR
     movingWindow_ = false;
 #endif
     QToolBar::mouseReleaseEvent(event);
@@ -34,7 +37,7 @@ QUToolBar::mouseReleaseEvent(QMouseEvent * event) {
 
 void
 QUToolBar::mouseDoubleClickEvent(QMouseEvent * event) {
-#ifdef Q_OS_MAC
+#ifdef QX_MOVE_WINDOW_BY_TOOLBAR
     if (this->window()->windowState().testFlag(Qt::WindowMaximized))
         this->window()->showNormal();
     else
