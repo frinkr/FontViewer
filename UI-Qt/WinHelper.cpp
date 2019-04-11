c4148ada77be2e94ada8d3a71410c4df046efba4
Daniel Jiang <frinkr@outlook.com>
2019-04-11 13:44:09 +0800

    Win32: show in explorer

--- UI-Qt/WinHelper.cpp
+++ UI-Qt/WinHelper.cpp
@@ -1,9 +1,13 @@
 #include <Shlobj.h>
+#include <QDir>
+#include <QFileInfo>
+#include <QString>
+#include <QProcess>
 #include "WinHelper.h"
 
 void
 WinHelper::showInExplorer(const QString & path) {
-    ITEMIDLIST *pidl = ILCreateFromPath(path.toStdWString().c_str());
+    ITEMIDLIST *pidl = ILCreateFromPathW(QDir::toNativeSeparators(path).toStdWString().c_str());
     if(pidl) {
         SHOpenFolderAndSelectItems(pidl,0,0,0);
         ILFree(pidl);
--- UI-Qt/WinHelper.h
+++ UI-Qt/WinHelper.h
@@ -2,6 +2,7 @@
 
 class QString;
 class WinHelper {
+public:
     static void
     showInExplorer(const QString & path);
 };
