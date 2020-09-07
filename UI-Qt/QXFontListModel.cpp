8743fdeda4185205eaf139fa571339f808fbc363
Daniel Jiang <frinkr@outlook.com>
2020-09-07 22:21:20 +0800

    font list search : fuzzy match

--- UI-Qt/QXFontListModel.cpp
+++ UI-Qt/QXFontListModel.cpp
@@ -7,45 +7,20 @@
 #include "QXDocument.h"
 #include "QXFontListModel.h"
 #include "QXFontManager.h"
+#include "score_match.h"
 
 namespace {
 
-    template<typename T, typename U> size_t
-    uiLevenshteinDistance(const T & s1, const U & s2) {
-        const size_t m(s1.size());
-        const size_t n(s2.size());
- 
-        if( m==0 ) return n;
-        if( n==0 ) return m;
- 
-        size_t *costs = new size_t[n + 1];
- 
-        for( size_t k=0; k<=n; k++ ) costs[k] = k;
- 
-        size_t i = 0;
-        for (auto it1 = s1.begin(); it1 != s1.end(); ++it1, ++i ) {
-            costs[0] = i+1;
-            size_t corner = i;
- 
-            size_t j = 0;
-            for (auto it2 = s2.begin(); it2 != s2.end(); ++it2, ++j){
-                size_t upper = costs[j+1];
-                if( *it1 == *it2 ) {
-                    costs[j+1] = corner;
-                }
-                else {
-                    size_t t(upper<corner?upper:corner);
-                    costs[j+1] = (costs[j]<t?costs[j]:t)+1;
-                }
- 
-                corner = upper;
-            }
-        }
- 
-        size_t result = costs[n];
-        delete [] costs;
- 
-        return result;
+    double
+    fuzzMatch(const QString& str, const QString& filter) {
+        auto s = str.toStdString();
+        auto f = filter.toStdString();
+
+        MatchOptions opt;
+        opt.case_sensitive = true;
+        opt.max_gap = 10;
+        opt.smart_case = true;
+        return score_match(s.c_str(), nullptr, f.c_str(), nullptr, opt);
     }
 
     bool
@@ -54,15 +29,8 @@ namespace {
         QString f = filter.toLower();
         if (s.contains(filter))
             return true;
-        if (filter.length() < string.length()) {
-            s.resize(f.size());
-            size_t ed = uiLevenshteinDistance(s, f);
-            return ed < f.length() / 2;
-        }
-        else {
-            size_t ed = uiLevenshteinDistance(s, f);
-            return ed < 9;
-        }
+
+        return fuzzMatch(s, f) > 0.3;
     }
 
     /**
@@ -73,31 +41,8 @@ namespace {
         QString f = filter.fontName.toLower();
         QString str0 = left.toLower();
         QString str1 = right.toLower();
-        
-        bool s0 = str0.startsWith(f);
-        bool s1 = str1.startsWith(f);
-        bool c0 = str0.contains(f);
-        bool c1 = str1.contains(f);
 
-        if (s0 != s1)
-            return s0;
-
-        if (c0 != c1)
-            return c0;
-
-        //QRegExp sep("\\s+");
-        QString sep(" ");
-        size_t ed0 = uiLevenshteinDistance(str0.splitRef(sep, QString::SkipEmptyParts), f.splitRef(sep, QString::SkipEmptyParts));
-        size_t ed1 = uiLevenshteinDistance(str1.splitRef(sep, QString::SkipEmptyParts), f.splitRef(sep, QString::SkipEmptyParts));
-        if (ed0 != ed1)
-            return ed0 < ed1;
-
-        ed0 = uiLevenshteinDistance(str0, f);
-        ed1 = uiLevenshteinDistance(str1, f);
-        if (ed0 == ed1)
-            return str0 < str1;
-        return ed0 < ed1;
-            
+        return fuzzMatch(str0, f) < fuzzMatch(str1, f);
     }
 }
 
@@ -140,13 +85,16 @@ QXFontListModel::acceptRow(const QXFontListFilter & filter, int row) const {
     
     // Check names
     auto const & atts = attributes(row);
-    auto fileName = QFileInfo(QString::fromStdString(atts.desc.filePath)).baseName();
+    auto filePath = QString::fromStdString(atts.desc.filePath);
+    auto fileName = QFileInfo(filePath).fileName();
     const bool acceptFontName = filter.fontName.isEmpty() ||
         acceptWithFilter(displayName(row), filter.fontName) ||
         acceptWithFilter(fileName, filter.fontName) ||
+        acceptWithFilter(filePath, filter.fontName) ||
         searchInNames(atts.sfntNames.localizedFamilyNames(), filter.fontName) ||
         searchInNames(atts.sfntNames.localizedStyleNames(), filter.fontName) ||
-        searchInNames(atts.sfntNames.localizedPostscriptNames(), filter.fontName);
+        searchInNames(atts.sfntNames.localizedPostscriptNames(), filter.fontName) 
+        ;
     
     if (!acceptFontName)
         return false;
